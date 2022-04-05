a6b6b6f33e46b87bd9690a1f4549c39c9f80d09d
Bogdannb34 <bogdan22bogdan@gmail.com>
2022-04-05 18:05:54 +0300

    v0.3 - ship updates

---
+++ VoyagesAPI/Interfaces/IShipRepo.cs
@@ -0,0 +1,17 @@
+﻿using VoyagesAPI.Entities;
+
+namespace VoyagesAPI.Interfaces
+{
+    public interface IShipRepo
+    {
+        Task<bool> AddShipAsync(Ship ship);
+
+        Task<IEnumerable<Ship>> GetAllAsync();
+
+        Task<Ship> GetByIdAsync(int id);
+
+        Task<bool> UpdateShipAsync(Ship ship);
+
+        Task<bool> DeleteShipAsync(int id);
+    }
+}
